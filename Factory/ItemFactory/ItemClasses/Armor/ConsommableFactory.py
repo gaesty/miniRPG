95710dec5b8e8b9de1257c0ffc5ade7d96de9d42
Ewnn <boulay.ewenn@gmail.com>
2026-02-13 11:27:31 +0100

    fix armor item classes and add new strategy

--- Factory/ItemFactory/ItemClasses/Armor/ConsommableFactory.py
+++ Factory/ItemFactory/ItemClasses/Armor/ConsommableFactory.py
@@ -1,15 +1,12 @@
 from CapeClass import CapeClass
 from LeatherArmorClass import LeatherArmorClass
 from MisticDressClass import MisticDressClass
-from SteelArmorClass import SteelArmorClass
 
 
 class ArmorFactory:
     def createArmor(self, name):
         if name == "leatherarmor":
             return LeatherArmorClass()
-        elif name == "steelarmor":
-            return SteelArmorClass()
         elif name == "misticdress":
             return MisticDressClass()
         elif name == "cape":
