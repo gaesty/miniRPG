145ccb96c0f56713af7279c58ba46409e8b18d9c
Ewnn <boulay.ewenn@gmail.com>
2026-02-13 11:42:56 +0100

    Add Inventory Strategy

---
+++ Strategy/InventoryStrategy/InventoryClasses/AddItemStrategy.py
@@ -0,0 +1,15 @@
+from InventoryStrategy.InventoryStrategy import InventoryStrategy
+
+class AddItemStrategy(InventoryStrategy):
+    def execute_action(self, inventory, item):
+        if "items" not in inventory:
+            inventory["items"] = []
+        
+        inventory["items"].append(item)
+        print(f"Item '{item.get('name', 'Unknown')}' added to inventory!")
+        print(f"Total items: {len(inventory['items'])}")
+        return True
+    
+    def get_strategy_name(self):
+        return "Add Item"
+
