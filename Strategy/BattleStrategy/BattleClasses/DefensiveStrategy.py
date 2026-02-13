8604c8be859df6b28295b36d3d7ef672dcc43e99
Ewnn <boulay.ewenn@gmail.com>
2026-02-13 11:18:41 +0100

    Add BattleStrategy and BattleClasses

---
+++ Strategy/BattleStrategy/BattleClasses/DefensiveStrategy.py
@@ -0,0 +1,15 @@
+from BattleStrategy.BattleStrategy import BattleStrategy
+
+class DefensiveStrategy(BattleStrategy):
+    def execute_attack(self, attacker, defender):
+        damage = attacker.get("offensive_stat_1", attacker.get("offensive_stat", 0)) // 2
+        defense = defender.get("defensive_stat_1", defender.get("defensive_stat", 0))
+        final_damage = max(damage - defense, 0)
+        defender["pv"] -= final_damage
+        attacker["defensive_stat_1"] = attacker.get("defensive_stat_1", attacker.get("defensive_stat", 0)) + 5
+        print(f"{attacker['name']} uses defensive strategy!")
+        print(f"{attacker['name']} attacks carefully for {final_damage} damage and increases defense!")
+        return final_damage
+    
+    def get_strategy_name(self):
+        return "Defensive"
---
+++ Strategy/BattleStrategy/BattleStrategy.py
@@ -0,0 +1,6 @@
+class BattleStrategy:
+    def execute_attack(self, attacker,defender):
+        pass
+
+    def get_strategy_name(self):
+        pass
